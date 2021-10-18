807fa9ddcd0589497894344fe0a8756bad36197b
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-10-18 19:08:01 +0800

    update q1

--- Practical/p1/q1.py
+++ Practical/p1/q1.py
@@ -4,9 +4,9 @@ class Customer:
         self.__email = email
         self.__mobile_number = mobile_number
 
-    def get_customer_info(self):
+    def get_info(self):
         return f'Name: {self.__name}, Email: {self.__email}, Mobile Number: {self.__mobile_number}'
 
 
 customer = Customer('Haiyun', 'haiyun@gmail.com', 98765432)
-print(customer.get_customer_info())
+print(customer.get_info())
