b143a8967d6d8af02769bb887a24cd01d01dac1f
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-10-31 16:25:04 +0800

    update p1 - q7

--- Practical/p1/q7.py
+++ Practical/p1/q7.py
@@ -1,46 +1,34 @@
 from q7_phone import Phone
 
-phones = {}
-
-
-def get_phone(phone_id):
-    if phone_id in phones:
-        phone = phones[phone_id]
-        return phone
-    else:
-        return None
-
-
-def display_all():
-    if len(phones) > 0:
-        for phone_id in phones:
-            print(phones[phone_id])
-    else:
-        print('There is no phone')
+phone_dict = {}
 
 
 def search():
-    phone_id = int(input('Enter phone id to search: '))
-    phone = get_phone(phone_id)
-    if phone is not None:
+    phone_id = input('Enter phone id to search: ')
+    if phone_id in phone_dict:
+        phone = phone_dict[phone_id]
         print(phone)
     else:
         print('Phone not found')
 
 
 def add():
-    make = input('Enter the phone make: ')
-    model = input('Enter the phone model: ')
-    price = input('Enter the phone price: ')
-    phone = Phone(make, model, float(price))
-    phones[phone.get_id()] = phone
-    print(f'The phone is added: {phone}')
+    phone_id = input('Enter the phone id: ')
+    if phone_id not in phone_dict:
+        make = input('Enter the phone make: ')
+        model = input('Enter the phone model: ')
+        price = input('Enter the phone price: ')
+        phone = Phone(phone_id, make, model, float(price))
+        phone_dict[phone_id] = phone
+        print(f'The phone is added: {phone}')
+    else:
+        print('Phone id already exists')
 
 
 def update():
-    phone_id = int(input('Enter phone id to update: '))
-    phone = get_phone(phone_id)
-    if phone is not None:
+    phone_id = input('Enter phone id to update: ')
+    if phone_id in phone_dict:
+        phone = phone_dict[phone_id]
         make = input('Enter the new make (leave empty to remain unchanged): ')
         model = input('Enter the new model (leave empty to remain unchanged): ')
         price = input('Enter the new price (leave empty to remain unchanged): ')
@@ -56,32 +44,38 @@ def update():
 
 
 def delete():
-    phone_id = int(input('Enter phone id to delete: '))
-    phone = get_phone(phone_id)
-    if phone is not None:
-        del phones[phone_id]
+    phone_id = input('Enter phone id to delete: ')
+    if phone_id in phone_dict:
+        phone = phone_dict[phone_id]
+        del phone_dict[phone_id]
         print(f'The phone is deleted: {phone}')
     else:
         print('Phone not found')
 
 
+def display_all():
+    if len(phone_dict) > 0:
+        for phone_id in phone_dict:
+            print(phone_dict[phone_id])
+    else:
+        print('No phone available')
+
+
 def display_menu():
     print('--------------------------')
     print('Phone Management System')
-    print('0 - Display all')
     print('1 - Search')
     print('2 - Add')
     print('3 - Update')
     print('4 - Delete')
-    print('5 - Quit')
+    print('5 - Display all')
+    print('6 - Quit')
     command = int(input('Enter the command (1-5): '))
     return command
 
 
 while True:
     command = display_menu()
-    if command == 0:
-        display_all()
     if command == 1:
         search()
     elif command == 2:
@@ -91,6 +85,8 @@ while True:
     elif command == 4:
         delete()
     elif command == 5:
+        display_all()
+    elif command == 6:
         print('Bye')
         break
     print()
