9dec33404420b0073e3240cc5080786848b009ed
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-10-31 16:52:47 +0800

    Update user.py

--- FlaskWebApp/user.py
+++ FlaskWebApp/user.py
@@ -1,26 +1,23 @@
 class User:
-    count = 0
-
     type_public = 'public'
     type_staff = 'staff'
 
     status_active = 1
     status_deleted = 0
 
-    def __init__(self, email, password, name):
-        User.count += 1
-        self.__id = User.count
-        self.__email = email
-        self.__password = password
-        self.__name = name
-        self.__type = User.type_public
-        self.__status = User.status_active
+    def __init__(self, user_id, email, password, name):
+        self.id = user_id
+        self.email = email
+        self.password = password
+        self.name = name
+        self.type = User.type_public
+        self.status = User.status_active
 
     def __str__(self):
-        return f'ID: {self.__id}\nEmail: {self.__email}\nName: {self.__name}\nType: {self.__type}\nStatus: {self.__status}\n'
+        return f'ID: {self.id}\nEmail: {self.email}\nName: {self.name}\nType: {self.type}\nStatus: {self.status}\n'
 
 
-user1 = User('user1@test.com', 'flaskwebapp', 'Alice')
+user1 = User(1, 'user1@test.com', 'flaskwebapp', 'Alice')
 print(user1)
-user2 = User('user2@test.com', 'flaskwebapp', 'Bryan')
+user2 = User(2, 'user2@test.com', 'flaskwebapp', 'Bryan')
 print(user2)
