81859af504650c5e8a877fad21dabae264f12959
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-11-03 21:44:21 +0800

    add email and password

--- FlaskWebApp/forms.py
+++ FlaskWebApp/forms.py
@@ -1,7 +1,12 @@
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
+from wtforms import Form, StringField, PasswordField, RadioField, SelectField, TextAreaField, validators
 
 
 class CreateUserForm(Form):
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.Length(min=6, max=15), validators.DataRequired()])
+    confirm_password = PasswordField('Confirm Password', [validators.Length(min=6, max=15), validators.DataRequired(),
+                                                          validators.EqualTo('password',
+                                                                             message='Passwords must match.')])
     name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     gender = SelectField('Gender', [validators.DataRequired()],
                          choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
