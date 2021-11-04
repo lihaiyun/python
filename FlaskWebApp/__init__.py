9b70d0e14bcf545876ded21aaaedb961113ec882
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-11-04 15:00:39 +0800

    save user to shelve

--- FlaskWebApp/__init__.py
+++ FlaskWebApp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import CreateUserForm
+from user import User
+import shelve
 
 app = Flask(__name__)
 
@@ -13,6 +15,24 @@ def home():
 def create_user():
     create_user_form = CreateUserForm(request.form)
     if request.method == 'POST' and create_user_form.validate():
+        user_dict = {}
+        db = shelve.open('library')
+        if 'users' in db:
+            user_dict = db['users']
+
+        email =  create_user_form.email.data
+        password = create_user_form.password.data
+        name = create_user_form.name.data
+        gender = create_user_form.gender.data
+        membership = create_user_form.membership.data
+        remarks = create_user_form.remarks.data
+        user = User(email, password, name, gender, membership, remarks)
+        print(user)
+
+        user_dict[user.id] = user
+        db['users'] = user_dict
+        db.close()
+
         return redirect(url_for('home'))
     return render_template('createUser.html', form=create_user_form)
 
--- FlaskWebApp/user.py
+++ FlaskWebApp/user.py
@@ -8,22 +8,24 @@ class User:
     status_active = 1
     status_deleted = 0
 
-    def __init__(self, email, password, name, gender, member=None, remarks=None):
+    def __init__(self, email, password, name, gender, membership=None, remarks=None):
         self.id = str(uuid.uuid4())
         self.email = email
         self.password = password
         self.name = name
         self.gender = gender
-        self.member = member
+        self.membership = membership
         self.remarks = remarks
         self.type = User.type_public
         self.status = User.status_active
 
     def __str__(self):
-        return f'ID: {self.id}\nEmail: {self.email}\nName: {self.name}\nGender: {self.gender}\nType: {self.type}\nStatus: {self.status}\n'
+        return f'ID: {self.id}\nEmail: {self.email}\nName: {self.name}\nGender: {self.gender}\n' \
+               f'Membership: {self.membership}\nRemarks:{self.remarks}\n' \
+               f'Type: {self.type}\nStatus: {self.status}\n'
 
 
-user1 = User('user1@test.com', 'flaskwebapp', 'Alice', 'F')
-print(user1)
-user2 = User('user2@test.com', 'flaskwebapp', 'Bryan', 'M')
-print(user2)
+# user1 = User('user1@test.com', 'flaskwebapp', 'Alice', 'F')
+# print(user1)
+# user2 = User('user2@test.com', 'flaskwebapp', 'Bryan', 'M')
+# print(user2)
