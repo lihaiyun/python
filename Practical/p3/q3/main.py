7e56abd829704059943b50fc091ca146919ae23d
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-11-11 14:36:24 +0800

    use monster name

--- Practical/p3/q3/main.py
+++ Practical/p3/q3/main.py
@@ -2,10 +2,10 @@ from fire_monster import FireMonster
 from water_monster import WaterMonster
 from grass_monster import GrassMonser
 
-firebug = FireMonster()
-waterbird = WaterMonster()
-grasshopper = GrassMonser()
+m1 = FireMonster('charmander')
+m2 = WaterMonster('squirtle')
+m3 = GrassMonser('bulbasaur')
 
-print(firebug)
-print(waterbird)
-print(grasshopper)
+print(m1)
+print(m2)
+print(m3)
