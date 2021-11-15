95b8ebc5c92ac7c6f5c21e936d78471c474b1bd4
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-11-15 11:09:20 +0800

    use display_info

--- Practical/p3/q3/main.py
+++ Practical/p3/q3/main.py
@@ -1,3 +1,4 @@
+from monster import Monster
 from fire_monster import FireMonster
 from water_monster import WaterMonster
 from grass_monster import GrassMonser
@@ -6,6 +7,19 @@ m1 = FireMonster('charmander')
 m2 = WaterMonster('squirtle')
 m3 = GrassMonser('bulbasaur')
 
-print(m1)
-print(m2)
-print(m3)
+# print(m1)
+# print(m2)
+# print(m3)
+
+
+def display_info(monster):
+    if isinstance(monster, Monster):
+        print(monster)
+    else:
+        print('Invalid monster')
+
+
+display_info(m1)
+display_info(m2)
+display_info(m3)
+display_info('Test')
