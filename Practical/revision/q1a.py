441e3f7aea4b83843da1acfa47d41df6bf3fef1a
Haiyun <li_haiyun@nyp.edu.sg>
2021-11-19 13:41:53 +0800

    delete unused files

--- Practical/revision/q1a.py
+++
@@ -1,5 +0,0 @@
-num_list = []
-for i in range(3):
-    num = int(input(f'Enter integer {i+1}: '))
-    num_list.append(num)
-print(f'Sum is {sum(num_list)}')
--- Practical/revision/q2b.py
+++
@@ -1,5 +0,0 @@
-from list import read_input
-
-num_list = read_input()
-print(num_list)
-print('End of program')
