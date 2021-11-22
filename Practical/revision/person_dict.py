aeb3066b1824ca2989a25b9e35a9f72f71e264ea
Li Haiyun <li_haiyun@nyp.edu.sg>
2021-11-22 20:49:37 +0800

    define functions

--- Practical/revision/person_dict.py
+++ Practical/revision/person_dict.py
@@ -1,36 +1,59 @@
+import shelve
 from person import Person
 
 person_dict = {}
 
-# Add person
-# for i in range(2):
-#     nric = input('Enter NRIC: ')
-#     name = input('Enter name: ')
-#     p = Person(nric, name)
-#     person_dict[nric] = p
 
-try:
-    file = open('person.txt', 'r')
-    lines = file.readlines()
-    for line in lines:
-        values = line.split(',')
-        nric = values[0].strip()
-        name = values[1].strip()
+# Read data from txt
+def read_from_txt():
+    try:
+        file = open('person.txt', 'r')
+        lines = file.readlines()
+        for line in lines:
+            values = line.split(',')
+            nric = values[0].strip()
+            name = values[1].strip()
+            p = Person(nric, name)
+            person_dict[nric] = p
+    except:
+        print('Error to open file')
+
+
+# Add person
+def add_person():
+    for i in range(2):
+        nric = input('Enter NRIC: ')
+        name = input('Enter name: ')
         p = Person(nric, name)
         person_dict[nric] = p
-except:
-    print('Error to open file')
 
-# display all person
-for p in person_dict.values():
-    print(p)
 
-# find person
-nric = input('Enter NRIC to find: ')
-found = None
-if nric in person_dict:
-    found = person_dict[nric]
-if found is not None:
-    print(f'Person found: {found}')
-else:
-    print('Person not found')
+# Display all person
+def display_all():
+    for p in person_dict.values():
+        print(p)
+
+# Find person
+def find_person():
+    nric = input('Enter NRIC to find: ')
+    found = None
+    if nric in person_dict:
+        found = person_dict[nric]
+    if found is not None:
+        print(f'Person found: {found}')
+    else:
+        print('Person not found')
+
+
+# Save data to shelve
+def save_to_shelve():
+    db = shelve.open('person')
+    db['person'] = person_dict
+    db.close()
+
+
+read_from_txt()
+# add_person()
+# display_all()
+# find_person()
+save_to_shelve()
--- Practical/revision/person_list.py
+++ Practical/revision/person_list.py
@@ -2,44 +2,61 @@ from person import Person
 
 person_list = []
 
-# Read data
-try:
-    file = open('person.txt', 'r')
-    lines = file.readlines()
-    for line in lines:
-        values = line.split(',')
-        nric = values[0].strip()
-        name = values[1].strip()
-        p = Person(nric, name)
-        person_list.append(p)
-except:
-    print('Error to open file')
+
+# Read data from txt
+def read_from_txt():
+    try:
+        file = open('person.txt', 'r')
+        lines = file.readlines()
+        for line in lines:
+            values = line.split(',')
+            nric = values[0].strip()
+            name = values[1].strip()
+            p = Person(nric, name)
+            person_list.append(p)
+    except:
+        print('Error to open file')
+
 
 # Add person
-for i in range(2):
-    nric = input('Enter NRIC: ')
-    name = input('Enter name: ')
-    p = Person(nric, name)
-    person_list.append(p)
+def add_person():
+    for i in range(2):
+        nric = input('Enter NRIC: ')
+        name = input('Enter name: ')
+        p = Person(nric, name)
+        person_list.append(p)
 
-# display all person
-for p in person_list:
-    print(p)
 
-# find person
-nric = input('Enter NRIC to find: ')
-found = None
-for p in person_list:
-    if p.nric == nric:
-        found = p
-        break
-if found is not None:
-    print(f'Person found: {found}')
-else:
-    print('Person not found')
+# Display all person
+def display_all():
+    for p in person_list:
+        print(p)
 
-# Save data
-file = open('person.txt', 'w')
-for p in person_list:
-    file.write(f'{p.nric},{p.name}\n')
-file.close()
+
+# Find person
+def find_person():
+    nric = input('Enter NRIC to find: ')
+    found = None
+    for p in person_list:
+        if p.nric == nric:
+            found = p
+            break
+    if found is not None:
+        print(f'Person found: {found}')
+    else:
+        print('Person not found')
+
+
+# Save data to txt
+def save_to_txt():
+    file = open('person.txt', 'w')
+    for p in person_list:
+        file.write(f'{p.nric},{p.name}\n')
+    file.close()
+
+
+read_from_txt()
+# add_person()
+# display_all()
+# find_person()
+save_to_txt()
