2e4c0a21457c59a4ff0b1a543526eedfb63a7401
Haiyun <li_haiyun@nyp.edu.sg>
2021-11-22 12:42:44 +0800

    find person

--- Practical/revision/person_dict.py
+++ Practical/revision/person_dict.py
@@ -2,11 +2,23 @@ from person import Person
 
 person_dict = {}
 
+# Add person
 for i in range(2):
     nric = input('Enter NRIC: ')
     name = input('Enter name: ')
     p = Person(nric, name)
     person_dict[nric] = p
 
+# display all person
 for p in person_dict.values():
     print(p)
+
+# find person
+nric = input('Enter NRIC to find: ')
+found = None
+if nric in person_dict:
+    found = person_dict[nric]
+if found is not None:
+    print(f'Person found: {found}')
+else:
+    print('Person not found')
--- Practical/revision/person_list.py
+++ Practical/revision/person_list.py
@@ -2,11 +2,25 @@ from person import Person
 
 person_list = []
 
+# Add person
 for i in range(2):
     nric = input('Enter NRIC: ')
     name = input('Enter name: ')
     p = Person(nric, name)
     person_list.append(p)
 
+# display all person
 for p in person_list:
     print(p)
+
+# find person
+nric = input('Enter NRIC to find: ')
+found = None
+for p in person_list:
+    if p.nric == nric:
+        found = p
+        break
+if found is not None:
+    print(f'Person found: {found}')
+else:
+    print('Person not found')
